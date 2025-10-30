736f6850c5c67787bf6bd84a6bd45987c5f57485
Toby267 <tobyhothersall@gmail.com>
2025-10-30 15:23:04 +0000

    Added Raylib

--- Win3D/src/Main.cpp
+++ Win3D/src/Main.cpp
@@ -1,5 +1,17 @@
-#include <iostream>
+#include "raylib.h"
 
 int main(int argc, char *argv[]) {
-    std::cout << "hello" << '\n';
+    InitWindow(600, 400, "Hello Raylib!");
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("Welcome to Raylib!", 190, 200, 20, LIGHTGRAY);
+        EndDrawing();
+    }
+
+    CloseWindow();
+
+    return 0;
 }
