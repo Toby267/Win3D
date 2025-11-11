cbedbeb659d61206ee3d55bd56ae4b0a31346a1d
Toby267 <tobyhothersall@gmail.com>
2025-11-11 13:46:23 +0000

    Comments

--- Win3D/src/graphicsPipeline/internals/GeometryProcessor.cpp
+++ Win3D/src/graphicsPipeline/internals/GeometryProcessor.cpp
@@ -17,19 +17,24 @@ void PipelineInternals::processGeometry(const Scene& scene, Bitmap3D& bitmap, co
         //step 2 - transform the camera to the origin, and take all objects along with it
         obj.applyTransformation(scene.getCamera().getTransformation());
 
-        //step 3 - vertex shading
+        //step 5 - tesselation
+        //this has to happen after step 2 as the z values need to be different. different z values result in different levels of tesselation
+        //but it has to happen before the vertex shader so that we have more detail from lighting in the vertex shading stage. for now, we just copy/interpolate vertex colours
+        
+        //step 4 - vertex shading
         //since we have transformed everything to 'camera space', we can assume that the camera is facing: (0, 0, 1)
         //although we haven't transformed light vectors... so this has to go above. or we just transform light vectors as well, because we are taking copies of the Object3Ds
+        
 
-        //step 4 - projection to the canonical view cube
+        //step 5 - projection to the canonical view cube
         obj.applyTransformation(scene.getCamera().getProjection());
 
-        //step 5 - clipping
+        //step 6 - clipping
 
-        //step 6 - screen mapping
+        //step 7 - screen mapping
         obj.applyTransformation(viewport.getTransformation());
         
-        //step 7 - the resulting data is passed onto the rasterization stage
+        //step 8 - the resulting data is passed onto the rasterization stage
         rasterize(bitmap, obj);
     }
 }
