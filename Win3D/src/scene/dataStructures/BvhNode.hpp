f05128e393ed210803011d871f0f620580f0e3d8
Toby267 <tobyhothersall@gmail.com>
2026-01-26 13:58:29 +0000

    designed the better bvh node

--- Win3D/src/scene/dataStructures/BvhNode.hpp
+++ Win3D/src/scene/dataStructures/BvhNode.hpp
@@ -1,40 +1,37 @@
 #pragma once
 
+#include "renderer/Ray.hpp"
 #include "scene/objects/Mesh.hpp"
 #include "scene/dataStructures/Aabb.hpp"
 #include "scene/core/SceneUtil.hpp"
 
 #include <vector>
 
-#define MIN_SIZE 4
-
-//why don't i make it so that each mesh has an accelerated data structure instead of the scene having one...
-
-//has instance(s) of either left, right, left & right, or just data
-//always has an aabb
 class BvhNode {
+public:
+    BvhNode(std::vector<Triangle>& triangles, int maxDepth, int minTriangles);
+    ~BvhNode();
+
+    // Triangle hit(const Ray&, float& t, float& u, float& v) const; // this is the preferred method, becuase it decouples ray tracing (triangle interpolation) logic from hit logic
+    bool hit(const Ray& ray, TrianglePoint& triangle, float& t) const;
+    void print() const;
+
 private:
     Aabb boundingBox;
 
     BvhNode* left = nullptr;
     BvhNode* right = nullptr;
 
-    Mesh* data = nullptr;
+    std::vector<Triangle> triangles;
 
-    //store copies of the meshes triangle(s) for this node
-    // std::vector<Triangle> triangles;
 
+
+
+
+//delete the following after implementing the better version
 public:
-    //constructors/destructor
-    BvhNode(std::vector<Mesh*> objects); //constructs a bvh tree on a mesh by mesh basis
-    BvhNode(std::vector<Triangle> triangles); // constructs a bvh tree on a triangle by traingle basis 
-    ~BvhNode();
-
-    //public methods
-    bool hit(const Ray& ray, TrianglePoint& triangle, float& t) const;
-    void print() const;
-
+    BvhNode(std::vector<Mesh*> objects);//delete
 private:
-    //private methods
-    float sweepSurfaceAreaHeuristic(std::vector<Mesh*>& axisOrderedObjects, int i);
+    Mesh* data = nullptr;//delete
+    float sweepSurfaceAreaHeuristic(std::vector<Mesh*>& axisOrderedObjects, int i);//delete
 };
