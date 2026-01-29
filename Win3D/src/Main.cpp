5d2c3b9d5b1149b597b5bc518c946bb9ddefbade
Toby267 <tobyhothersall@gmail.com>
2026-01-29 11:51:07 +0000

    can now open a .obj file

--- Win3D/src/Main.cpp
+++ Win3D/src/Main.cpp
@@ -1,9 +1,12 @@
 #include "Application.hpp"
+#include "util/WavefrontLoader.hpp"
 
 //Driver code
 int main(int argc, char *argv[]) {
-    Application* app = new Application();
-    delete app;
+    Wavefront::loadWavefront("Cube.obj");
+    
+    // Application* app = new Application();
+    // delete app;
 
     return 0;
 }
--- Win3D/src/util/wavefrontLoader.cpp
+++ Win3D/src/util/wavefrontLoader.cpp
@@ -1,12 +1,24 @@
 #include "util/WavefrontLoader.hpp"
 #include "scene/core/SceneUtil.hpp"
-#include "scene/dataStructures/Aabb.hpp"
-#include <vector>
+#include "scene/objects/Mesh.hpp"
+#include <cassert>
+#include <fstream>
+#include <iostream>
+#include <string>
 
+// assumes path to start from ./resources
 Mesh* Wavefront::loadWavefront(std::string path) {
     IndexBuffer indexBuffer{};
     VertexBuffer vertexBuffer{};
-    
-    std::vector<Triangle> triangles{};
-    std::vector<Triangle> normaLS{};
+
+    std::ifstream obj(RESOURCES_PATH + path);
+    assert(obj);
+
+    std::string s;
+    while (std::getline(obj, s))
+        std::cout << s << '\n';
+
+    obj.close();
+
+    return new Mesh(indexBuffer, vertexBuffer);
 }
