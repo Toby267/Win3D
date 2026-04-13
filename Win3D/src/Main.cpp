90577595ce625ff17ab0a5044c62c01f1e3fecbe
Toby267 <tobyhothersall@gmail.com>
2026-04-13 10:07:48 +0100

    can now choose the scene from the terminal

--- Win3D/src/Main.cpp
+++ Win3D/src/Main.cpp
@@ -1,10 +1,34 @@
 #include "Application.hpp"
 #include "engine/Engine.hpp"
+#include <cassert>
+#include <iostream>
+
+/*
+scenes:
+- bunny1440_400
+*/
 
 //Driver code
 int main(int argc, char *argv[]) {
-    Engine e = Engine(400, 400);
-    Application app = Application(e);
+    assert(argc == 1 || argc == 2);
+    
+    // default scene
+    if (argc == 1) {
+        Engine e = Engine(400, 400);
+        Application app = Application(e);
+
+        return 0;
+    }
+
+    // other scenes
+    std::string scene = argv[1];
+    if (scene == "bunny1440_400") {
+        Engine e = Engine(400, 400);
+        Application app = Application(e);
+    }
+    else {
+        std::cerr << "Invalid scene ID" << '\n';
+    }
 
     return 0;
 }
